62c70f893a5b815f56a359718302c40a30a90d50
sergfer26 <sfernandezm97@ciencias.unam.mx>
2019-08-16 14:28:49 -0500

    Add files via upload

--- Board.py
+++ Board.py
@@ -1,11 +1,24 @@
 import numpy as np
+import gym
+from gym import spaces
 
-class Board: 
+
+class TicTacToe(gym.Env): 
+    metadata = {'render.modes': ['human']}
 
     def __init__(self):
-        self.items = ['']*9
-        self.canon_items = 
-        self.syms = {'': 0, 'O': 1, 'X': 2}
+        self.action_space = spaces.Discrete(9)
+        # self.observation_space = spaces.Discrete(9)
+        self.items = self.reset()
+        self.state = [0]*9
+        self.winner = None
+        self.syms = {'': 0, 'O': 2, 'X': 1}
+
+    def reset(self):
+        board = [''] * 9
+        self.done = False
+        self.winner = None
+        return board
 
     def show_board(self):
         board = """
@@ -17,24 +30,74 @@ class Board:
         """.format(*self.items)
         print(board)
 
-    def mark_(self, space, mark='X'):
-        if self.items[space] == '':
-            self.items[space] = mark
-        else:
-            print('espacio ocupado')
+    def mark_(self, action, mark):
+            if self.items[action] == '':
+                self.items[action] = mark
+            else:
+                print('espacio ocupadp')
 
     def items_to_state(self):
         '''
             pasa los elementos de tablero a un estado
         '''
-        state = [self.syms.get(sym) for sym in self.items]
-        return state
+        self.state = [self.syms.get(sym) for sym in self.items]
 
-    def state_to_items(self, state):
+    def state_to_items(self):
         '''
             recibe un estado y actualiza los elemetos del tablero
         '''
         self.items = [self.syms[key] for key in state]
+
+    def is_X_winner(self, x, y, z):
+        if x == y == z and x == 'X':
+            return 1
+        elif x == y == z and x == 'O':
+            return 2
+        else:
+            return -1
+
+    def is_game_over(self):
+        '''
+        Regresa el status del juego.
+            -1: El juego esta en progreso
+             0: El juego quedo empatado
+             1: Gana X
+             2: Gana O  
+        '''
+        items = self.items
+        for i in range(0, 3):
+            j = 3*i
+            status = self.is_X_winner(items[i], items[i+3], items[i+6])
+            status = self.is_X_winner(items[j], items[j+1], items[j+2])
+
+        status = self.is_X_winner(items[0], items[4], items[8])
+        status = self.is_X_winner(items[2], items[4], items[6])
+
+        if '' not in self.items and status == -1:
+            status = 0
+
+        return status
+
+    def step(self, action):
+        reward = 0.0
+        self.mark_(action, 'X')
+        status = self.is_game_over()
+        if status >= 0:
+            self.done = True
+            if status == 1:
+                reward = 1.0
+        self.items_to_state()
+        while True:
+            action_O = self.action_space.sample()
+            if self.state[action_O] == 0:
+                break
+        self.mark_(action_O, 'O')
+        self.items_to_state()
+        return self.state, reward, self.done
+
+
+
+ 
 
     
 
---
+++ agent.py
@@ -0,0 +1,134 @@
+from Board import TicTacToe
+import gym
+import collections
+import numpy as np
+
+
+class Agent:
+  
+    def __init__(self):
+        '''
+            Reward table: (source state , action, target state) -> inmediate 
+            reward
+            Transitions table : (state, action) -> { states_counter}
+            Value table: state -> value.
+        '''
+        self.board = TicTacToe()
+        self.state = self.board.state
+        self.rewards = collections.defaultdict(float)
+        self.transits = collections.defaultdict(collections.Counter)
+        self.values = collections.defaultdict(float)
+
+    def play_n_random_steps(self, n):
+        '''
+            Realiza n turnos con jugadas aleatorias para conocer 
+            estados posibles
+        '''
+        for _ in range(n):
+            while True:
+                action = self.board.action_space.sample()
+                if self.state[action] == 0:
+                    break 
+
+            new_state, reward, is_done = self.board.step(action)
+            key = self.get_min_state(self.state)
+            new_key = self.get_min_state(new_state)
+            self.rewards[(key, action, new_key)] = reward
+            self.transits[(key, action)][new_key] += 1
+            self.state = self.board.reset() if is_done else new_state
+
+    def state_to_matrix(self, state):
+        '''
+            Pasa el estado a un 1%arreglo de arreglos
+            1x9 -> 3x3 .
+        '''
+        # -1 infers the size of the new dimension from 
+        # the size of the input array
+        return np.reshape(state, (-1, 3))
+
+    def rotate_right(self, state):
+        '''
+            Da el estado de rotar el tablero en el sentido horario 
+            de acuerdo a las columnas del tablero. 
+        '''
+        mat = self.state_to_matrix(state)
+        aux = np.zeros((3, 3), dtype=int)
+        aux[:, 2] = mat[0, ]
+        aux[:, 1] = mat[1, ]
+        aux[:, 0] = mat[2, ]
+        state = aux.reshape(-1, 9)[0, ]
+        return state
+
+    def reflect(self, state):
+        '''
+            Intercambia la columna 3 por la 1 (Reflexión de abajo horizontal).
+        '''
+        a = state[0:3]
+        b = state[3:6]
+        c = state[6:]
+        aux = np.concatenate((b, a), axis=None)
+        state = np.concatenate((c, aux), axis=None)
+        return state
+
+    def state_equal(self, state1, state2):
+        '''
+        Verifica de si dos estados son equivalentes.
+        '''
+        if self.is_rotated(state1, state2):
+            return True
+        else:
+            state1 = self.reflect(state1)
+            if self.is_rotated(state1, state2):
+                return True
+        return False
+
+    def is_rotated(self, state1, state2):
+        '''
+            Verifica sin un estado es equivalente a otro aplicando rotaciones.
+        '''
+        for i in range(4):
+            if np.array_equal(state1, state2):
+                return True
+            state1 = self.rotate_right(state1)
+        return False
+
+    def state_to_base10(self, state):
+        '''
+            Transforma el vector de estados de base 3 a un número de base 10.
+        '''
+        powers = np.array(range(9))
+        vec = np.array([3]*9)
+        base = np.power(vec, powers)
+        return np.dot(base, state)
+
+    def base10_to_state(self, num):
+        '''
+            Pasa el número de base 10 al estado asociado.
+        '''
+        digits = []
+        base = 3
+        while num > 0:
+            digits.insert(0, num % base)
+            num = num // base
+        digits.reverse()
+        state = np.array(digits)
+        return state
+ 
+    def get_min_state(self, state):
+        '''
+            Obtine el minimo valor de estado asociado 
+        '''
+        states = collections.defaultdict(list)
+        key = self.state_to_base10(state)
+        states[key] = state 
+        new_state = state
+        for j in range(2):
+            for i in range(4):
+                new_state = self.rotate_right(new_state)
+                new_key = self.state_to_base10(new_state)
+                states[new_key] = new_state
+            new_state = self.reflect(new_state)
+        return min(states.items())
+
+
+
