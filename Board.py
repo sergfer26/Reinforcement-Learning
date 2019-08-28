e32549f5e58e0e6055e8e930b4f4fd520d238928
sergfer26 <sfernandezm97@ciencias.unam.mx>
2019-08-28 11:34:30 -0500

    Add files via upload
    modifique el método ``step()`` y modifique el método ``state_to_items()``

--- Board.py
+++ Board.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import numpy as np
 import gym
 from gym import spaces
 
@@ -10,11 +9,15 @@ class TicTacToe(gym.Env):
     def __init__(self):
         self.action_space = spaces.Discrete(9)
         # self.observation_space = spaces.Discrete(9)
-        self.items = ['']*9
-        self.state = [0]*9
+        self.done = False
+        self.items = [''] * 9
+        self.state = [0] * 9
         self.syms = {'': 0, 'O': 2, 'X': 1}
 
     def reset(self):
+        '''
+            Establece las condiciones iniciales del juego.
+        '''
         self.items = [''] * 9
         self.state = [0] * 9
         self.done = False
@@ -22,6 +25,9 @@ class TicTacToe(gym.Env):
         
 
     def show_board(self):
+        '''
+            Muestra el estado del juego para el usuario.
+        '''
         board = """
              {} | {} | {}
             ----------
@@ -32,34 +38,41 @@ class TicTacToe(gym.Env):
         print(board)
 
     def mark_(self, action, mark):
-            if self.items[action] == '':
-                self.items[action] = mark
-            else:
-                print('espacio ocupadp')
+        '''
+            Marca alguna casilla del tabler
+        '''
+        if self.items[action] == '':
+            self.items[action] = mark
+        else:
+            print('espacio ocupadp')
 
     def items_to_state(self):
         '''
-            pasa los elementos de tablero a un estado
+            Pasa los elementos de tablero a un estado.
         '''
-        self.state = [self.syms.get(sym) for sym in self.items]
+        self.state = [self.syms[key] for key in self.items]
 
     def state_to_items(self):
         '''
-            recibe un estado y actualiza los elemetos del tablero
+            Recibe un estado y actualiza los elemetos del tablero.
         '''
-        self.items = [self.syms[key] for key in self.state]
+        reversed_syms = {value: key for key, value in self.syms.items()}
+        self.items = [reversed_syms[value] for value in self.state]
 
     def is_X_winner(self, x, y, z):
-        if x == y == z and x == 'X':
-            return 1
-        elif x == y == z and x == 'O':
-            return 2
+        if x == y == z:
+            if x == 'X':
+                return 1
+            elif x == 'O':
+                return 2
+            else:
+                return -1
         else:
             return -1
 
     def is_game_over(self):
         '''
-        Regresa el status del juego.
+            Regresa el status del juego.
             -1: El juego esta en progreso
              0: El juego quedo empatado
              1: Gana X
@@ -79,7 +92,7 @@ class TicTacToe(gym.Env):
 
         status = self.is_X_winner(items[0], items[4], items[8])
         if status in [1, 2]:
-                return status
+            return status
 
         status = self.is_X_winner(items[2], items[4], items[6])
 
@@ -87,9 +100,29 @@ class TicTacToe(gym.Env):
             return 0
 
         return status
+
+    def step(self, action, mark):
+        '''
+            Recibe una acción de un jugador y realiza el 
+            movimiento
+        '''
+        reward = 0.0
+        self.mark_(action, mark)
+        status = self.is_game_over()
+        if status >= 0:
+            self.done = True
+            if status == 1:
+                reward = 1.0
+        self.items_to_state()
+        print('------------ turno de ', mark, '------------')
+        self.show_board()
+        return self.state, reward, self.done
         
-    def step(self, action):
-        self.done = False
+    '''def step(self, action):
+        
+            Dada la acción realiza el movimiento del jugador 'X' 
+            y la respuesta del jugador 'O'.
+        
         reward = 0.0
         self.mark_(action, 'X')
         status = self.is_game_over()
@@ -109,4 +142,4 @@ class TicTacToe(gym.Env):
             self.mark_(action_O, 'O')
             self.items_to_state()
             self.show_board()
-        return self.state, reward, self.done
+        return self.state, reward, self.done'''
