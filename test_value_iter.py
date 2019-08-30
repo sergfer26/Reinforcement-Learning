9e9d23c6568b64edd17967c321c7848f00075245
sergfer26 <sfernandezm97@ciencias.unam.mx>
2019-08-30 13:39:37 -0500

    Add files via upload

---
+++ test_value_iter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from board import TicTacToe as ttt
+from agent_value_iteration import Agent_value_iteration as agent_vi
+from tensorboardX import SummaryWriter
+
+if __name__ == "__main__":
+    TE = 20  # test episodes
+    test_board = ttt()
+    player = agent_vi()
+    writer = SummaryWriter(comment='v-learning')
+    i = 0
+    best_reward = 0.0
+    while True:
+        i += 1
+        player.play_n_random_steps(1000)
+        player.value_iteration()
+        reward = 0.0 
+        for _ in range(TE):
+            reward += player.play_episode(test_board)
+            reward /= TE
+            writer.add_scalar('reward', reward, i)
+            if reward > best_reward:
+                print("Best reward updated % .3f -> %.3f" % (best_reward, reward))
+                best_reward = reward
+            if reward > 0.80:
+                print("Solved in  %d iteration!" % i)
+                break 
+    writer.close()
+
