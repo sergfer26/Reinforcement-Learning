ef2c3b54c813909a42f80cee293151e6c118aa81
Sergio Fernández <sergiofernandez@Sergios-MacBook-Pro.local>
2019-10-25 00:15:18 -0500

    se corrigió la impresión de acciones posibles

--- human.py
+++ human.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 from base_agent import BaseAgent
 
+
 class Human(BaseAgent):
-    def select_action(self,key):
-        state = self.key_to_state(key)
+
+    def select_action(self, key, ref, rots):
+        state = self.minstate_to_realstate(key, ref, rots)
         actions = [i for i, e in enumerate(state) if e == 0]
         print(actions)
         action = int(input('Tu jugada es: '))
