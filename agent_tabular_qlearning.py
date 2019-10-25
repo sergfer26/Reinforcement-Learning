2ecdf63880d81e3bd6a73ef044c8f5215d8d2bdf
Sergio Fernández <sergiofernandez@Sergios-MacBook-Pro.local>
2019-10-25 00:10:24 -0500

    se actualizo la función q

--- agent_tabular_qlearning.py
+++ agent_tabular_qlearning.py
@@ -2,6 +2,9 @@
 from base_agent import BaseAgent
 import collections
 
+ALPHA = 0.5
+GAMMA = 0.5
+
 
 class Agent_TQL(BaseAgent):
     '''
@@ -11,9 +14,6 @@ class Agent_TQL(BaseAgent):
     def __init__(self):
         BaseAgent.__init__(self)
         self.values = collections.defaultdict(float) 
-        self.alpha = 0.5
-        self.gamma = 0.5
-        self.key = 0
 
     def update_dicts(self, reflected, rots, player, k):
         '''
@@ -48,16 +48,17 @@ class Agent_TQL(BaseAgent):
                     break
             self.key = self.reset_key()
 
-    def sample_env(self, reflected, rots, player):
+    def sample_env(self, ref, rots, player):
         state = self.key_to_state(self.key)
         action = self.select_random_action(state)
-        board_action = self.get_board_action(action, reflected, rots)
+        board_action = self.get_board_action(action, ref, rots)
+        print('esta es la acción: ', board_action, action)
         old_key = self.key
         new_state, reward, is_done = self.board.step(board_action, player)
         new_key = self.get_min_state(new_state)[0]
-        [_, reflected, rots] = self.get_min_state(new_state)[1]
+        [_, ref, rots] = self.get_min_state(new_state)[1]
         self.key = self.reset_key if is_done else new_key
-        return old_key, action, reward, new_key, reflected, rots, is_done
+        return old_key, action, reward, new_key, ref, rots, is_done
 
     def best_value_and_action(self, key):
         best_value, best_action = None, None
@@ -74,10 +75,14 @@ class Agent_TQL(BaseAgent):
         best_value, _ = self.best_value_and_action(nk)
         old_val = self.values[(k, a)]
         if best_value:
-            new_val = r + self.gamma * best_value - old_val
+            new_val = r + GAMMA * best_value - old_val
         else: 
-            new_val = - old_val
-        self.values[(k, a)] = old_val  + self.alpha * new_val 
+            new_val = r - old_val
+        self.values[(k, a)] = old_val + ALPHA * new_val 
+
+    def select_action(self, key):
+        _, a = self.best_value_and_action(key)
+        return a
 
     def play_episode(self, board):
         total_reward = 0.0
@@ -99,3 +104,23 @@ class Agent_TQL(BaseAgent):
             key = new_key
             k += 1
         return total_reward
+
+    def get_step_info(self, key, action, reward, new_key):
+        self.value_update(key, action, reward, new_key)
+
+if __name__ == "__main__":
+    import json
+    import base_agent as ba
+    PLAYERS = ['X', 'O']
+    player = Agent_TQL()
+    player.values = ba.remap_stringkeys_rewards(json.load(open("qvalues.txt")))
+    rots = 0
+    ref = False
+    i = 0
+    while True:
+        k, a, r, nk, ref, rots, done = player.sample_env(ref, rots, PLAYERS[i % 2])
+        player.value_update(k, a, r, nk)
+        i += 1
+        if done:
+            print(player.values[(k, a)])
+            break
