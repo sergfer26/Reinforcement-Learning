0ac270e26d8215903a02c06bb9a2fa3c2743064d
Sergio Fernández <sergiofernandez@Sergios-MacBook-Pro.local>
2019-10-25 00:12:55 -0500

    se actualizo duel

--- duel.py
+++ duel.py
@@ -1,29 +1,46 @@
 from board import Board
+from human import Human as human
+import json
 
 board = Board()
 PLAYERS = ['X', 'O']
+rewards = json.load(open('rewards.txt'))
+
+
 def duel(agent_X, agent_O, show=False):
     board.reset()
     done = False
     k = 0
     key = 0
-    reflected = False
+    ref = False
     rots = 0
     while not done:
         player = PLAYERS[k % 2]
+
         if k % 2 == 0:
             agent = agent_X
-        elif k % 2 != 0: 
+        elif k % 2 != 0:
             agent = agent_O
+
         if show:
-            print(reflected,rots)
             board.show_board()
-        action = agent.select_action(key)
-        board_action = agent.get_board_action(action, reflected, rots)
-        state, reward, done = board.step(board_action, player)
-        key = agent.get_min_state(state)[0]
-        [_, reflected, rots] = agent.get_min_state(state)[1]
-        k +=1
+
+        if isinstance(agent, human):
+            action = agent.select_action(key, ref, rots)
+            board_action = action
+        else: 
+            action = agent.select_action(key)
+            board_action = agent.get_board_action(action, ref, rots)
+
+        new_state, reward, done = board.step(board_action, player)
+        new_key = agent.get_min_state(new_state)[0]
+        [_, ref, rots] = agent.get_min_state(new_state)[1]
+        k += 1
+        agent_X.get_step_info(key, action, reward, new_key)
+        agent_O.get_step_info(key, action, -reward, new_key)
+        key = new_key
+
     board.show_board()
     return board.is_game_over()
 
+    # def do_n_duels(n, agent_X, agent_O):
