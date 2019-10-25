4c593fe12574b348c9dd7367b9d97b8d1cf6551e
Sergio Fernández <sergiofernandez@Sergios-MacBook-Pro.local>
2019-10-25 00:14:15 -0500

    queda pendiente borrarlo

--- duel_value_it_vs_random.py
+++ duel_value_it_vs_random.py
@@ -4,16 +4,19 @@ from board import Board
 from agent import AgentVI
 import json
 from human import Human
-from base_agent import remap_stringkeys_rewards,remap_stringkeys_transits 
+from minmax_agent import MinMax_Agent as mma
+from base_agent import remap_stringkeys_rewards, remap_stringkeys_transits
 
 player_X = AgentVI()
 
 player_X.transits = remap_stringkeys_transits(json.load(open("transits.txt")))
-player_X.rewards =  remap_stringkeys_rewards(json.load(open("rewards.txt")))
-player_X.values =  remap_stringkeys_rewards(json.load(open("values.txt")))
+player_X.rewards = remap_stringkeys_rewards(json.load(open("rewards.txt")))
+player_X.values = remap_stringkeys_rewards(json.load(open("values.txt")))
 
+player_X = mma()
+player_X.rewards = remap_stringkeys_rewards(json.load(open("rewards.txt")))
+player_X.minmax()
 
 player_O = Human()
-player_O = AgentVI()
 
-print(duel(player_X,player_O,show = True))
+print(duel(player_X, player_O, show=True))
