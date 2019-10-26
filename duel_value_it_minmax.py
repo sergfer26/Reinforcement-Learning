9e36c29b7b2374e7c8a250b34111ef3ed951b2f8
Sergio Fernández <sergiofernandez@Sergios-MacBook-Pro.local>
2019-10-26 08:33:06 -0500

    se quito transits

--- duel_value_it_minmax.py
+++ duel_value_it_minmax.py
@@ -8,7 +8,7 @@ from base_agent import remap_stringkeys_rewards, remap_stringkeys_transits
 
 player_X = AgentVI()
 
-player_X.transits = remap_stringkeys_transits(json.load(open("transits.txt")))
+#player_X.transits = remap_stringkeys_transits(json.load(open("transits.txt")))
 player_X.rewards = remap_stringkeys_rewards(json.load(open("rewards.txt")))
 player_X.values = remap_stringkeys_rewards(json.load(open("values.txt")))
 
