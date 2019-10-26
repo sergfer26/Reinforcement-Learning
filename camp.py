3c8b0a5b8e3eeb61789ed51b8f651e5253b4f675
Sergio Fernández <sergiofernandez@Sergios-MacBook-Pro.local>
2019-10-25 23:26:03 -0500

    se agrego formato

--- camp.py
+++ camp.py
@@ -1,38 +1,39 @@
 #!/usr/bin/env python3
 from duel import duel
 from board import Board
-from agent import AgentVI
 from agent_random import Random
-from agent_Value_iter import Value_iter
+from agent_value_iteration import AgentVI as avi
+from agent_value_iteration import create_avi
 from human import Human
 
-Agent1 = Value_iter()
+Agent1 = create_avi()
 Agent2 = Random()
 Agent3 = Human()
-A1,A2,A3 = 0,0,0
-Agentes = [Agent1,Agent2,Agent3]
-names = ['Value_iteration', 'Aleatorio','Humano']
+A1, A2, A3 = 0, 0, 0
+Agents = [Agent1, Agent2, Agent3]
+names = ['Value_iteration', 'Aleatorio', 'Humano']
 
-def un_duelo(X,O,varX,varO):
-    gana = 1 if duel(X,O,show = True) == 1 else 0
+
+def un_duelo(X, O, varX, varO):
+    gana = 1 if duel(X, O, show=True) == 1 else 0
     if gana == 1:
         varX += 1
     else:
         varO += 1
-    return varX,varO
+    return varX, varO
 
-A1,A2 = un_duelo(Agent1,Agent2,A1,A2)
-A1,A3 = un_duelo(Agent1,Agent3,A1,A3)
 
-A2,A1 = un_duelo(Agent2,Agent1,A2,A1)
-A2,A3 = un_duelo(Agent2,Agent3,A2,A3)
+A1, A2 = un_duelo(Agent1, Agent2, A1, A2)
+A1, A3 = un_duelo(Agent1, Agent3, A1, A3)
 
-A3,A2 = un_duelo(Agent2,Agent2,A3,A2)
-A3,A1 = un_duelo(Agent3,Agent1,A3,A1)
+A2, A1 = un_duelo(Agent2, Agent1, A2, A1)
+A2, A3 = un_duelo(Agent2, Agent3, A2, A3)
 
+A3, A2 = un_duelo(Agent2, Agent2, A3, A2)
+A3, A1 = un_duelo(Agent3, Agent1, A3, A1)
 
 
 print('Puntuacion final:')
-print(names[0] + ' '+ str(A1))
-print(names[1] + ' '+str(A2))
-print(names[2] + ' '+str(A3))
+print(names[0] + ' ' + str(A1))
+print(names[1] + ' ' + str(A2))
+print(names[2] + ' ' + str(A3))
--- interpreter.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-class Interpreter():
-    
-
-    def mat_to_csv(name, lista):
-        df = pd.DataFrame(data=mat.astype(float))
-        df.to_csv(name, sep=' ', header=False, float_format='%.2f', index=False)
