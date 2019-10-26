748cbe9b03b4484798f6a98d1a0cf72b43b8f5e7
Sergio Fernández <sergiofernandez@Sergios-MacBook-Pro.local>
2019-10-25 23:28:52 -0500

    se arreglo select_action

--- human.py
+++ human.py
@@ -5,7 +5,14 @@ from base_agent import BaseAgent
 class Human(BaseAgent):
 
     def select_action(self, key, ref, rots):
-        state = self.minstate_to_realstate(key, ref, rots)
+        state = self.key_to_state(key)
+        for _ in range(rots):
+            state = self.rotate_left(state)
+        if ref:
+            if rots % 2 == 0:
+                state = self.reflect_h(state)
+            else: 
+                state = self.reflect_v(state)
         actions = [i for i, e in enumerate(state) if e == 0]
         print(actions)
         action = int(input('Tu jugada es: '))
