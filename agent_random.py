4fe7cc47395d915c1ee61fd30b2566c6b95a8aa9
Sergio Fernández <sergiofernandez@Sergios-MacBook-Pro.local>
2019-10-27 20:57:27 -0600

    se dio formato

--- agent_random.py
+++ agent_random.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 from base_agent import BaseAgent
 from numpy.random import choice
+
+
 class Random(BaseAgent):
-    def select_action(self,key):
+
+    def select_action(self, key):
         state = self.key_to_state(key)
         actions = [i for i, e in enumerate(state) if e == 0]
         action = choice(actions)
