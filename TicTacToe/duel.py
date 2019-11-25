639a6f00e43d388d3d5412cbb9656898a393ed17
Sergio Fernández <sergiofernandez@Sergios-MacBook-Pro.local>
2019-11-25 07:03:13 -0600

    se cambio a modo no determinista

--- TicTacToe/duel.py
+++ TicTacToe/duel.py
@@ -6,51 +6,65 @@ import numpy as np
 import pandas as pd
 
 
-board = board()
-PLAYERS = ['X', 'O']
-
-
-def duel(agent_X, agent_O, show=False):
-    board.reset()
+def duel(playerX, playerO, show=False, board=board()):
+    playerX.role = 'X'
+    playerO.role = 'O'
     done = False
     i = 0
     key = 0
     ref = False
     rots = 0
-    while not done:
-        player = PLAYERS[i % 2]
-        if i % 2 == 0:
-            agent = agent_X
-        elif i % 2 != 0:
-            agent = agent_O
-
+    old_key = None
+    old_action = None
+    player = playerX
+    while True:
         if show:
             board.show_board()
-        if isinstance(agent, human):
-            action = agent.select_action(key, ref, rots)
+        if isinstance(player, human):
+            action = player.select_action(key, ref, rots)
             board_action = action
         else:
-            action = agent.select_action(key)
-            board_action = agent.get_board_action(action, ref, rots)
+            action = player.select_action(key)
+            board_action = player.get_board_action(action, ref, rots)
 
-        new_state, reward, done = board.step(board_action, player)
-        new_key = agent.get_min_state(new_state)[0]
-        [_, ref, rots] = agent.get_min_state(new_state)[1]
+        new_state, reward, done = board.step(board_action, player.role)
+        new_key = player.get_min_state(new_state)[0]
+        [_, ref, rots] = player.get_min_state(new_state)[1]
         i += 1
-        agent_X.get_step_info(key, action, reward, new_key)
-        agent_O.get_step_info(key, action, reward, new_key)
+        if old_key:
+            if player.role == 'X':
+                reward = -reward
+
+            playerX.get_step_info(old_key, old_action, reward, new_key)
+            playerO.get_step_info(old_key, old_action, reward, new_key)
+
+        old_key = key
         key = new_key
-    
+        old_action = action
+
+        if i % 2 == 0:
+            player = playerX
+        else:
+            player = playerO
+
+        if done:
+            reward = -reward
+            playerX.get_step_info(old_key, old_action, reward, None)
+            playerO.get_step_info(old_key, old_action, reward, None)
+            break
+
     if show:
         board.show_board()
 
-    return board.is_game_over()
+    winner_value = board.is_game_over()
+    board.reset()
+    return winner_value
 
 
 def play_n_duels(games, agent1, agent2, show=False):
     playerX = agent1
     playerO = agent2
-    prob = []
+    frec = []
     draws = 0
     for k in range(1, games+1):
         winner_value = duel(playerX, playerO, show)
@@ -59,14 +73,14 @@ def play_n_duels(games, agent1, agent2, show=False):
             aux = playerX
             playerX = playerO
             playerO = aux
-            playerX.set_role('X')
-            playerO.set_role('O')
+            playerX.role = 'X'
+            playerO.role = 'O'
         elif winner_value == 1:
             playerX.wins += 1 
         elif winner_value == 0:
             draws += 1 
 
         if k % 100 == 0:
-            prob.append(agent2.wins/k)
+            frec.append((agent2.wins+draws)/k)
 
-    return np.array(prob)
+    return np.array(frec)
