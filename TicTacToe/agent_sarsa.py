6d2edf7e6aa82705fd8e154a966cf0f5ca7ae68a
Sergio Fernández <sergiofernandez@Sergios-MacBook-Pro.local>
2019-11-25 07:00:11 -0600

    se actualizo get_info

--- TicTacToe/agent_sarsa.py
+++ TicTacToe/agent_sarsa.py
@@ -1,12 +1,4 @@
 from .agent_tabular_qlearning import Agent_TQL
-from .read_tables import QVALUES
-
-
-
-def create_asarsa():
-    agent = Agent_SARSA()
-    agent.values = QVALUES
-    return agent
 
 
 class Agent_SARSA(Agent_TQL):
@@ -19,10 +11,6 @@ class Agent_SARSA(Agent_TQL):
         self.SAR = {'state': 0, 'action': 0, 'reward': 0}
 
     def get_step_info(self, key, action, reward, new_key):
-        if self.role == 'X':
-            pass
-        else: 
-            reward = -reward
 
         if key != 0:
             na = action
@@ -36,14 +24,11 @@ class Agent_SARSA(Agent_TQL):
         self.SAR['action'] = action
         self.SAR['reward'] = reward
 
-        if reward != 0:
+        if not new_key:
             self.reset_SAR()
             self.value_update(ns, na, reward, None, None)
 
     def value_update(self, s, a, r, ns, na):
         old_val = self.values[(s, a)]
-        if na:
-            new_val = r + self.gamma * self.values[(ns, na)] - old_val
-        else:
-            new_val = r - old_val
-        self.values[(s, a)] = old_val + self.alpha * new_val
+        new_val = r + self.gamma * self.values[(ns, na)] - old_val
+        self.values[(s, a)] += self.alpha * new_val
