b391d6fd8fb21088294233ba55864991d9aef471
Sergio Fernández <sergiofernandez@Sergios-MacBook-Pro.local>
2019-11-25 07:04:47 -0600

    se necesita actualización o borrarse

--- TicTacToe/read_tables.py
+++ TicTacToe/read_tables.py
@@ -2,12 +2,21 @@ import json
 import os
 from collections import defaultdict
 from ast import literal_eval
+import collections
 
 
 def remap_keys(mapping, type_=float):
     dic = defaultdict(type_)
     for k, v in mapping.items():
         dic[str(k)] = v
+    return dic
+
+
+def remap_transits(mapping, type_=collections.Counter):
+    dic = defaultdict(type_)
+    for key, val in mapping.items():
+        for k, v in val.items():
+            dic[str(key)][str(k)] = v
     return dic
 
 
@@ -26,22 +35,22 @@ def remap_values(mapping, type_=float):
     return dic
 
 
-tvalues_fn = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), 'tables/trained_values.txt')
-TVALUES = json.load(open(tvalues_fn))
-TVALUES = remap_stringkeys(TVALUES)
-
+# tvalues_fn = os.path.join(
+#     os.path.dirname(os.path.abspath(__file__)), 'tables/trained_values.txt')
+# TVALUES = json.load(open(tvalues_fn))
+# TVALUES = remap_stringkeys(TVALUES)
+# 
 rewards_fn = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), 'tables/rewards.txt')
+    os.path.dirname(os.path.abspath(__file__)), 'tables/minmax_rewards.txt')
 rewards = json.load(open(rewards_fn))
 REWARDS = remap_stringkeys(rewards)
-
-qvalues_fn = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), 'tables/qvalues.txt')
-qvalues = json.load(open(qvalues_fn))
-QVALUES = remap_stringkeys(qvalues)
-
-values_fn = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), 'tables/values.txt')
-rewards = json.load(open(values_fn))
-VALUES = remap_stringkeys(rewards)
+# 
+# qvalues_fn = os.path.join(
+#     os.path.dirname(os.path.abspath(__file__)), 'tables/qvalues.txt')
+# qvalues = json.load(open(qvalues_fn))
+# QVALUES = remap_stringkeys(qvalues)
+# 
+# values_fn = os.path.join(
+#     os.path.dirname(os.path.abspath(__file__)), 'tables/values.txt')
+# rewards = json.load(open(values_fn))
+# VALUES = remap_stringkeys(rewards)
