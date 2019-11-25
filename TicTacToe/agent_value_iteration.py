a343e2e616fbc6b36f860d56a5a97a856d7ef5cc
Sergio Fernández <sergiofernandez@Sergios-MacBook-Pro.local>
2019-11-25 07:01:46 -0600

    se actualizo get_info

--- TicTacToe/agent_value_iteration.py
+++ TicTacToe/agent_value_iteration.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-from .base_agent import BaseAgent
-from .read_tables import REWARDS, TVALUES, remap_stringkeys, remap_keys
-from .read_tables import remap_values
+import numpy as np
 import collections
 import json
+from TicTacToe.base_agent import BaseAgent
+from TicTacToe.read_tables import remap_stringkeys, remap_keys
+from TicTacToe.read_tables import remap_values, remap_transits
 from collections import Counter
-import numpy as np
 from numpy.linalg import norm as norma
+
+#REWARDS, TVALUES,
+PLAYERS = ['X', 'O']
 
 
 def create_avi():
     agent = AgentVI()
-    agent.rewards = REWARDS
-    agent.values = TVALUES
+    #agent.rewards = REWARDS
+    #agent.values = TVALUES
     return agent
 
 
@@ -32,60 +35,69 @@ class AgentVI(BaseAgent):
         self.key = 0
         self.rewards = collections.defaultdict(float)
         self.values = collections.defaultdict(float)
+        self.transits = collections.defaultdict(collections.Counter)
         self.gamma = 0.5
-    
-    def update_dicts(self, reflected, rots, player, k):
-        '''
-            Actualiza las diccionarios asociados a
-            las tablas de valores.
-        '''
-        state = self.key_to_state(self.key)
-        action = self.select_random_action(state)
-        board_action = self.get_board_action(action, reflected, rots)
-        new_state, reward, is_done = self.board.step(board_action, player)
-        new_key = self.get_min_state(new_state)[0]
-        [_, reflected, rots] = self.get_min_state(new_state)[1]
-        self.rewards[(self.key, action, new_key)] = reward
-        self.values[new_key] = 0
-        return is_done, new_key, reflected, rots
 
-    def play_n_random_games(self, n):
-        '''
-            Realiza n turnos con jugadas aleatorias para conocer 
-            estados posibles.
-        '''
-        self.values[self.key] = 0
+    def play_random_episode(self, show=False):
+        self.board.reset()
+        done = False
+        i = 0
         ref = False
         rots = 0
-        plyrs = ['X', 'O']
+        self.values[self.key] = 0
+        old_action = None
+        old_key = None
+
+        while not done:
+            self.set_role(PLAYERS[i % 2])
+
+            if show:
+                self.board.show_board()
+                
+            action = self.select_random_action(self.key)
+            board_action = self.get_board_action(action, ref, rots)
+            new_state, reward, done = self.board.step(board_action, self.role)
+            new_key = self.get_min_state(new_state)[0]
+            [_, ref, rots] = self.get_min_state(new_state)[1]
+            i += 1
+
+            if self.role == 'X':
+                reward = - reward
+
+            if self.key != 0:
+                self.rewards[(old_key, old_action, new_key)] = reward
+                self.transits[(old_key, old_action)][new_key] += 1
+
+            reward = -reward
+            self.values[new_key] = 0
+            old_key = self.key
+            old_action = action
+            self.key = new_key
+
+        self.rewards[(old_key, old_action, None)] = reward
+        self.transits[(old_key, old_action)][None] += 1
+        self.board.reset()
+        self.key = self.reset_key()
+
+    def play_n_random_games(self, n):
         for _ in range(n):
-            k = 0
-            while True:
-                is_done, nk, ref, rots = self.update_dicts(ref, rots, plyrs[k % 2], k)
-                self.key = self.reset_key() if is_done else nk
-                k += 1 
-                if is_done:
-                    ref, rots = self.reset_rr()
-                    break
-            self.key = self.reset_key()
+            self.play_random_episode()
 
     def calc_action_value(self, key, action):
-        rewards = self.rewards.keys() 
-        lista = [nk for k, a, nk in rewards if k == key and a == action]
-        if lista:
-            tgt_key = lista[0]
-        else:
-            print(action)
-            print(self.key_to_state(key))
+        target_counts = self.transits[(key, action)]
+        total = sum(target_counts.values())
+        action_value = 0.0
+        for tgt_key, count in target_counts.items():
+            reward = self.rewards[(key, action, tgt_key)]
+            action_value += (count / total) * (reward +
+            self.gamma * self.values[tgt_key])
 
-        reward = self.rewards[(key, action, tgt_key)]
-        action_value = reward + self.gamma * self.values[tgt_key]
         return action_value
 
     def select_action(self, key):
         best_action, best_value, = None, None
-        rewards = self.rewards.keys() 
-        actions = [a for k, a, nk in rewards if key == k]
+        state = self.key_to_state(key)
+        actions = [a for a, e in enumerate(state) if e == 0]
         for action in actions:
             action_value = self.calc_action_value(key, action)
             if best_value is None or best_value < action_value:
@@ -100,39 +112,13 @@ class AgentVI(BaseAgent):
         for key, _, _ in rewards: 
             state_vals = []
             val = self.values[key]
-            actions = [a for k, a, nk in rewards if key == k]
+            state = self.key_to_state(key)
+            actions = [a for a, e in enumerate(state) if e == 0]
             for action in actions:
                 state_vals.append(self.calc_action_value(key, action))  
             self.values[key] = max(state_vals) if len(state_vals) else val
 
-    def set_role(self, role):
-        if role != self.role:
-            self.role = role
-            self.reawards = remap_values(self.rewards)
-            self.values = remap_values(self.values)
-
-
-if __name__ == "__main__":
-
-    player = AgentVI()
-    matrix = []
-    player.play_n_random_games(10000)
-    json.dump(player.values, open("tables/values.txt", 'w'))
-    dim = len(player.values)
-    y = np.repeat(0, dim)
-    self.epsilon = 0.001
-    i = 0
-    while True:
-        player.value_iteration()
-        x = np.array(list(player.values.values()))
-        matrix.append(x)
-        i += 1
-        if norma(x-y) < self.epsilon:
-            break
-        y = x
-    matrix = np.flip(matrix, axis=0)
-    matrix.reshape((i, dim))
-    rewards = remap_keys(player.rewards)
-    json.dump(player.values, open("tables/trained_values.txt", 'w'))
-    json.dump(rewards, open("tables/rewards.txt", 'w'))
-    # d2 = json.load(open("text.txt"))
+    def get_step_info(self, key, action, reward, new_key):
+        self.transits[(key, action)][new_key] += 1
+        self.rewards[(key, action, new_key)] = reward
+        
